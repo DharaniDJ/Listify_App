37445ce800a1002fa902f529b92f41548497b5de
DharaniChinta <nvchinta@asu.edu>
2023-02-27 17:08:21 -0700

    password hashing

---
+++ hashing.py
@@ -0,0 +1,16 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"],deprecated=["auto"])
+
+class Hasher:
+
+    # Access methods with the class itself,
+    # no need of objects to use the methods
+
+    @staticmethod
+    def get_hash_password(plain_password):
+        return pwd_context(plain_password)
+
+    @staticmethod
+    def verify_password(plain_password,hash_password):
+        return pwd_context.verify(plain_password,hash_password)
