82ee4e83483ecaf631e3617146a11998d78aa956
DharaniDJ <dharani56525@gmail.com>
2023-03-04 01:01:27 -0700

    hidded unnecessary details from response(response_model)

--- routers/users.py
+++ routers/users.py
@@ -1,7 +1,7 @@
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from schemas import UserCreate
+from schemas import UserCreate, ShowUser
 from database import get_db
 from hashing import Hasher
 from models import User
@@ -12,7 +12,7 @@ router = APIRouter()
 def get_user():
     return {"message":"hello user"}
 
-@router.post('/users', tags=["user"])
+@router.post('/users', tags=["user"], response_model=ShowUser)
 def create_user(user : UserCreate, db:Session=Depends(get_db)):
     user = User(email=user.email, password=Hasher.get_hash_password(user.password))
     db.add(user)
--- schemas.py
+++ schemas.py
@@ -3,3 +3,10 @@ from pydantic import EmailStr, BaseModel
 class UserCreate(BaseModel):
     email : EmailStr
     password : str
+
+class ShowUser(BaseModel):
+    email: EmailStr
+    is_active: bool
+
+    class Config:
+        orm_mode =True
