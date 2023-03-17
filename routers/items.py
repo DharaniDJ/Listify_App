a958dc7353cfcd91690a8efe76e41288ed3ee456
DharaniChinta <nvchinta@asu.edu>
2023-03-17 15:51:57 -0700

    implemented item updation using put endpoint

--- routers/items.py
+++ routers/items.py
@@ -5,6 +5,7 @@ from models import Items
 from datetime import datetime
 from sqlalchemy.orm import Session
 from database import get_db
+from fastapi.encoders import jsonable_encoder
 
 router = APIRouter()
 
@@ -29,3 +30,13 @@ def retrieve_item_by_id(id:int, db:Session=Depends(get_db)):
     if not item:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Item {id} does not exists")
     return item
+
+@router.put("/item/update/{id}",tags=["items"])
+def update_item_by_id(id:int, item:ItemCreate,db:Session=Depends(get_db)):
+    existing_item = db.query(Items).filter(Items.id==id)
+    if not existing_item.first():
+        return {"message":f'No details exists for Item ID {id}'}
+
+    existing_item.update(jsonable_encoder(item)) # update keyword require jsonable_encoder, we can use item.__dict__ also
+    db.commit()
+    return {"message":f"Details for Item ID {id} successfully updated"}
