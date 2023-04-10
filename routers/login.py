2794da342f6094107affa3b9ac7d208dc26d5382
DharaniChinta <nvchinta@asu.edu>
2023-04-10 10:00:45 -0700

    logic for delete item using javasscript

--- routers/login.py
+++ routers/login.py
@@ -1,18 +1,19 @@
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Response
 from sqlalchemy.orm import Session
 from database import get_db
 from models import User
 from hashing import Hasher
 from jose import jwt
 from config import setting
+from utils import OAuth2PasswordBearerWithCookie
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="/login/token")
 
 router = APIRouter()
 
 @router.post("/login/token",tags=["login"])
-def retrieve_token_after_authentication(form_data:OAuth2PasswordRequestForm=Depends(), db:Session=Depends(get_db)):
+def retrieve_token_after_authentication(response:Response, form_data:OAuth2PasswordRequestForm=Depends(), db:Session=Depends(get_db)):
     user = db.query(User).filter(User.email==form_data.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid Username")
@@ -22,6 +23,7 @@ def retrieve_token_after_authentication(form_data:OAuth2PasswordRequestForm=Depe
         "sub":form_data.username
     }
     jwt_token = jwt.encode(data, setting.SECRET_KEY, algorithm=setting.ALGORITHM)
+    response.set_cookie(key="access_token",value=f"Bearer {jwt_token}", httponly=True)
     return {
         "access_token":jwt_token,
         "token_type":"bearer"
